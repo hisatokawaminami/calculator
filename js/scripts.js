0c60ac46ae9b5124821ced62adde574bde1c2741
Hisato Kawaminami and Nick Rogers <hisato@>
2018-05-29 10:59:00 -0700

    Create calculator folder.  Add scripts, html, and README.md.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var sum = function(number1, number2) {
+	return number1+number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert(sum(number1,number2));
